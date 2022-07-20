06180c20cda03358d18c0ccaebc1b60f45fa59c0
kelue <austincmoon5@gmail.com>
2022-07-20 18:41:20 +0000

    Wed, Jul 20, 2022, 6:41 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,7 +24,37 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0, hend = height - 1; i < hend; i++)
+    {
+        for (int j = 0, wend = width - 1; j < wend; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int blue = image[i][j].rgbtBlue;
+            int green = image[i][j].rgbtGreen;
+
+            int sepiaRed = round(.393 * red + .769 * green + .189 * blue);
+            int sepiaGreen = round(.349 * red + .686 * green + .168 * blue);
+            int sepiaBlue =  round(.272 * red + .534 * green + .131 * blue);
+
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            else if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            else if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+
+        }
+    }
 }
 
 // Reflect image horizontally
