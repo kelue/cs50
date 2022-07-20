d83e9dd7203c0395cca78ff78077359d7eede82b
kelue <austincmoon5@gmail.com>
2022-07-20 18:04:40 +0000

    Wed, Jul 20, 2022, 6:04 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,19 +1,22 @@
 #include "helpers.h"
-#include 
+#include <math.h>
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-
-    for (int i = 0, end = height -1; i < end; i++)
+    for (int i = 0, end = height - 1; i < end; i++)
     {
-        for (int j = 0, end = width -1; j < end; j++)
+        for (int j = 0, end = width - 1; j < end; j++)
         {
             int red = image[i][j].rgbtRed;
             int blue = image[i][j].rgbtBlue;
             int green = image[i][j].rgbtGreen;
 
-            int result = round
+            int result = round((red + blue + green)/3);
 
+            *image[i][j].rgbtRed = result;
+            *image[i][j].rgbtBlue = result;
+            *image[i][j].rgbtGreen = result;
         }
     }
 }
